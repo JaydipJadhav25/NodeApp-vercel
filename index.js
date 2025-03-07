e7d091c64a49bf3a622bbd9c02b0a0566019ebe6
jaydipjadhav <jaydipdjadhav>
2025-03-08 00:19:21 +0530

    update

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express from "express"
-
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, Vercel!");
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://127.0.0.1:${port}/`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
