1799f1aca92ec50b98259c9acbd02d4d121d52ab
jaydipjadhav <jaydipdjadhav>
2025-03-08 00:23:05 +0530

    add route

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get("/", (req, res) => {
   res.send("Hello, Vercel!");
 });
 
+app.get("/name/:name", (req, res) => {
+    const name = req.params.name; // Correctly extracting the route parameter
+    res.send(`Hello, ${name}!`);
+});
+
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
